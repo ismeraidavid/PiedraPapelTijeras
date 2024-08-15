dc585e7abb111d6d491b78172024349f9ca943ea
Ismerai <54740257+ismeraidavid@users.noreply.github.com>
2024-08-15 19:30:18 +0100

    Añadida la función para elegir la elección del jugador y la función para jugar una ronda entre el jugador y el ordenador.

--- script.js
+++ script.js
@@ -4,17 +4,90 @@ function getComputerChoice () {
     let dado = Math.random()
     if (dado >= 0 && dado <= 1/3) {
         let ComputerChoice = "Piedra";
-        return ComputerChoice;
+        return ComputerChoice.toLowerCase();
 
     } else if (dado > 1/3 && dado < 2/3) {
         let ComputerChoice = "Papel";
-        return ComputerChoice;
+        return ComputerChoice.toLowerCase();
 
     } else {
         let ComputerChoice = "Tijeras";
-        return ComputerChoice;
+        return ComputerChoice.toLowerCase();
     }
     
 }
 
-console.log(getComputerChoice())
+//console.log("Elección del ordenador: " + getComputerChoice())
+
+// Decisión del jugador
+
+function getHumanChoice (){
+    let HumanChoice = prompt("¿Cuál es tu elección?");
+    let HumanChoiceLower = HumanChoice.toLowerCase();
+    if (HumanChoiceLower === "piedra") {
+        return HumanChoiceLower;
+    } else if (HumanChoiceLower === "papel") {
+        return HumanChoiceLower;
+    } else if (HumanChoiceLower === "tijeras") {
+        return HumanChoiceLower;
+    } else {
+        let BadChoice = "Introduce una opción válida: piedra, papel o tijeras."
+        return BadChoice;
+    }
+}
+
+//console.log("Elección del jugador: " + getHumanChoice())
+
+// Sistema de puntos
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    // Si el jugador elige Piedra
+    if (humanSelection === "piedra") {
+        choiceHuman = "piedra"
+        if (humanSelection === computerSelection){
+            console.log(`¡Empate! Ambos habéis escogido: ${humanSelection} `);
+        } else if (humanSelection === choiceHuman && computerSelection === "papel") {
+            computerScore++;
+            return console.log (`¡Gana el ordenador! ${computerSelection} gana a ${humanSelection}`)    
+        } else if (humanSelection === choiceHuman && computerSelection === "tijeras") {
+            humanScore++;
+            return console.log (`¡Gana el humano! ${humanSelection} gana a ${computerSelection}`)
+        }
+    // Si el jugador elige Papel
+    } else if (humanSelection === "papel") {
+        choiceHuman = "papel"
+        if (humanSelection === computerSelection){
+            console.log(`¡Empate! Ambos habéis escogido: ${humanSelection} `);
+        } else if (humanSelection === choiceHuman && computerSelection === "tijeras") {
+            computerScore++;
+            return console.log (`¡Gana el ordenador! ${computerSelection} gana a ${humanSelection}`)    
+        } else if (humanSelection === choiceHuman && computerSelection === "piedra") {
+            humanScore++;
+            return console.log (`¡Gana el humano! ${humanSelection} gana a ${computerSelection}`)
+        }
+    // Si el jugador elige Tijeras
+} else if (humanSelection === "tijeras") {
+    choiceHuman = "tijeras"
+    if (humanSelection === computerSelection){
+        console.log(`¡Empate! Ambos habéis escogido: ${humanSelection} `);
+    } else if (humanSelection === choiceHuman && computerSelection === "piedra") {
+        computerScore++;
+        return console.log (`¡Gana el ordenador! ${computerSelection} gana a ${humanSelection}`)    
+    } else if (humanSelection === choiceHuman && computerSelection === "papel") {
+        humanScore++;
+        return console.log (`¡Gana el humano! ${humanSelection} gana a ${computerSelection}`)
+    }
+    
+
+
+}
+}
+
+playRound()
+console.log(`Marcador ---- HUMANO: ${humanScore} - ORDENADOR: ${computerScore}`);
